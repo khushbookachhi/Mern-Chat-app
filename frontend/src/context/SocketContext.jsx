87349dbac8dbbffe7e03a9620b15c1cac05e7111
khushboo <kkachhi178@gmail.com>
2024-06-27 19:48:13 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider=({children})=>{
     const {authUser}=useAuthContext()
     useEffect(()=>{
         if(authUser){
-            const socket=io("http://localhost:8030",{
+            const socket=io("https://mern-chat-app-0d0h.onrender.com",{
                 query:{
                     userId:authUser._id
                 }
